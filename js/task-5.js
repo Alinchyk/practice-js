2001d6846ae07ae6512b82b1a79a60561b15c251
Alinchyk <alina.mykhalchuk09@gmail.com>
2022-12-01 17:20:19 +0100

    add solution

---
+++ js/task-5.js
@@ -0,0 +1,30 @@
+/** ЗАДАЧА 5 - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ *  - строка
+ *  - число
+ *  - логическое
+ *  - null
+ *  - undefined
+ *  - объект
+ *  - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений
+ * используя оператор typeof
+ */
+
+const str = "name";
+const num = 10;
+const boole = true;
+const date = null;
+let date1;
+const obj = {};
+const arr = [];
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof boole);
+console.log(typeof date);
+console.log(typeof date1);
+console.log(typeof obj);
+console.log(typeof arr);
