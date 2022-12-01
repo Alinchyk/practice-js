1154eadc79d98ee305d2d3a38cab8d31fb3b08d7
Alinchyk <alina.mykhalchuk09@gmail.com>
2022-12-01 18:02:15 +0100

    add solution

---
+++ js/task-11.js
@@ -0,0 +1,19 @@
+/**
+ * 1. Создайте объект с тремя свойствами:
+ *  - name
+ *  - surname
+ *  - favoriteNumber
+ *
+ * 2. Выведите в консоль строку
+ * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
+ */
+
+const obj = {
+  name: "Alla",
+  surname: "Kowalska",
+  favoriteNumber: 3,
+};
+
+console.log(
+  `My name is ${obj.name} ${obj.surname} and my favorite number is ${obj.favoriteNumber}`
+);
